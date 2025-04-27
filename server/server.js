cc2c3c63d6db352647c5feb2a8a92bf0343cb578
Malky2005 <38327725693@mby.co.il>
2025-04-27 14:02:42 +0300

    basic server

---
+++ server/server.js
@@ -0,0 +1,28 @@
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const mongoose= require('mongoose')
+const corsOptions = require('./config/corsOptions')
+const connectDB = require('./config/DBConnection')
+
+const app = express()
+const PORT = process.env.PORT || 2244
+
+connectDB()
+app.use(cors(corsOptions))
+app.use(express.json())
+
+app.get('/',(req,res)=>{
+    res.send("hello")
+})
+
+mongoose.connection.once('open', ()=>{
+    console.log('connected to DB');
+    app.listen(PORT, ()=>{
+        console.log(`server running on port ${PORT}`);
+    })
+})
+
+mongoose.connection.on('error', err=>{
+    console.log(err);
+})
